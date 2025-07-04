bd1af1fa3ec10dc9534762d3f5dc24f3d911742f
roobasekarb <roobasekar55@gmail.com>
2025-07-04 21:25:19 +0530

    Add files via upload

---
+++ basic program.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	String name= "kavi";
+	String name1="namakkal";
+	System.out.println(name+" "+"technologies"+" "+name1);
+	}
+}
